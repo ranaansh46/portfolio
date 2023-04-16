f32a810be1556d2aeb860773b71e20f023d64474
ranaansh46 <ranaansh46@gmail.com>
2023-04-16 15:40:02 +0530

    portfolio first_Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let navb1 = document.getElementById("navbutton1");
+let navb2 = document.getElementById("navbutton2");
+let navb3 = document.getElementById("navbutton3");
+navb1.addEventListener('click',()=>{
+    window.open('https://twitter.com/ranaansh46', '_blank');
+})
+navb2.addEventListener('click',()=>{
+    window.open('https://medium.com/@ranaansh46', '_blank');
+})
+navb3.addEventListener('click',()=>{
+    window.open('https://www.youtube.com/@moon.ranaansh46/featured', '_blank');
+})
